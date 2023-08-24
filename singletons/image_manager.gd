c14cc2dd792508f56870df738ee916d7f5bdde05
heliosalesjr <heliosalesjr@gmail.com>
2023-08-24 12:12:40 -0300

    first commit / image loader

---
+++ singletons/image_manager.gd
@@ -0,0 +1,34 @@
+extends Node
+#guide: https://github.com/heliosalesjr/memoryMadnessGuide
+var _item_images: Array = []
+
+func _ready():
+	load_item_images()
+
+func add_file_to_list(fn: String, path: String) -> void:
+	var full_path = path + "/" + fn
+	
+	var ii_dict = {
+		"item_name": fn.rstrip(".png"),
+		"item_texture": load(full_path)
+	}
+	
+	_item_images.append(ii_dict)
+
+func load_item_images() -> void:
+	
+	#the following vars are there so we can access the folder and files
+	var path = "res://assets/glitch"
+	var dir = DirAccess.open(path)
+	
+	if not dir:
+		print("ERROR:", path)
+		return
+		
+	var file_names = dir.get_files()
+	
+	for fn in file_names:
+			if ".import" not in fn:
+				add_file_to_list(fn, path)
+				
+	
