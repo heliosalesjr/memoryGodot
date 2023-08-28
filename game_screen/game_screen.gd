e61102e12b9f5b0caf8dbb0eb360a1af09ac4584
heliosalesjr <heliosalesjr@gmail.com>
2023-08-28 17:24:25 -0300

    navigation system

---
+++ game_screen/game_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+@onready var sound = $Sound
+
+func _ready():
+	pass
+	
+
+func _on_exit_button_pressed():
+	SoundManager.play_button_click(sound)
+	SignalManager.on_game_exit_pressed.emit()
