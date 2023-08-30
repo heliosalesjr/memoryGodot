92d46b8ae871434bd26c33b73a5e3044e4317acd
heliosalesjr <heliosalesjr@gmail.com>
2023-08-30 15:15:51 -0300

    score and tile selection

--- singletons/signal_manager.gd
+++ singletons/signal_manager.gd
@@ -2,3 +2,6 @@ extends Node
 
 signal on_level_selected(level_num: int)
 signal on_game_exit_pressed
+signal on_selection_enabled
+signal on_selection_disabled
+signal on_tile_selected(tile: MemoryTile)
